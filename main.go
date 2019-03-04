4656bc881de5a037285811327a06799df2f28fd2
galih abdullah <galihabdullah471@gmail.com>
2019-03-04 23:13:04 +0700

    a

--- main.go
+++ main.go
@@ -1,36 +1,42 @@
 package main
 
+
 import (
 	"io/ioutil"
 	"log"
-	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"net/http"
 	"os"
 	"os/exec"
 	"strings"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func main() {
 	bot, err := tgbotapi.NewBotAPI("653629960:AAFizrqn043ird_bXJpMZmkDCtpSjyWfmDA")
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
 
 	bot.Debug = true
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates, err := bot.GetUpdatesChan(u)
+	_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert("https://www.google.com:8443/"+bot.Token, "cert.pem"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	info, err := bot.GetWebhookInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if info.LastErrorDate != 0 {
+		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+	}
+	updates := bot.ListenForWebhook("/" + bot.Token)
+	go http.ListenAndServeTLS("0.0.0.0:8443", "cert.pem", "key.pem", nil)
 
 	for update := range updates {
-		if update.Message == nil { // ignore any non-Message Updates
-			continue
-		}
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
+		log.Printf("%+v\n", update)
 		if strings.ContainsAny(update.Message.Text, "func"){
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 			message := []byte(update.Message.Text)
