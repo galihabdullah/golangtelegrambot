4093018e122b6e7875eba09ff8d25816951278d2
galih abdullah <galihabdullah471@gmail.com>
2019-03-05 14:28:57 +0700

    a

--- main.go
+++ main.go
@@ -1,47 +1,50 @@
 package main
 
-
 import (
+	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"os/exec"
 	"strings"
-	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+
 func main() {
-	bot, err := tgbotapi.NewBotAPI("653629960:AAFizrqn043ird_bXJpMZmkDCtpSjyWfmDA")
+	bot, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 
 	bot.Debug = true
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert("https://inlaid-vehicle-233108.appspot.com/"+bot.Token, "cert.pem"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	info, err := bot.GetWebhookInfo()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if info.LastErrorDate != 0 {
-		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
-	}
-	updates := bot.ListenForWebhook("/" + bot.Token)
-	go http.ListenAndServeTLS("0.0.0.0:8443", "cert.pem", "key.pem", nil)
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		log.Printf("%+v\n", update)
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
 		if strings.ContainsAny(update.Message.Text, "func"){
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 			message := []byte(update.Message.Text)
-			ioutil.WriteFile("livetest.go", message, os.ModePerm)
-			out, err := exec.Command("go", "run", "livetest.go").Output()
+			tmpFile, err := ioutil.TempFile("","example.go")
+			if err != nil{
+				log.Printf(err.Error())
+			}
+			defer os.Remove(tmpFile.Name())
+
+			if _, err = tmpFile.Write(message); err != nil{
+				log.Printf(err.Error())
+			}
+			out, err := exec.Command("go", "run", "example.go").Output()
 			if err != nil{
 				msg.Text = err.Error()
 			}else{
@@ -49,6 +52,9 @@ func main() {
 			}
 			bot.Send(msg)
 			log.Printf("To: %+v Text: %+v\n",msg.ReplyToMessageID, msg.Text)
+			if err := tmpFile.Close(); err != nil{
+				log.Printf(err.Error())
+			}
 		}
 	}
 }
